b32b104783a0b5f99812c53fd46888214011abc6
erica nogueira <ericasnogueira10@gmail.com>
2022-08-18 11:35:51 -0300

    Exercicio04

---
+++ src/EstruturaSequencial/Exericio04.java
@@ -0,0 +1,31 @@
+package EstruturaSequencial;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exericio04 {
+    public static void main(String[] args) {
+        /*
+        Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por
+        hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário,
+        com duas casas decimais.
+         */
+
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        int numeroFuncionario ;
+        double horas, salario, salarioFuncionario ;
+
+        numeroFuncionario = input.nextInt();
+        horas = input.nextDouble();
+        salario = input.nextDouble();
+
+        salarioFuncionario = horas * salario;
+
+        System.out.println("Número Funcionario = " + numeroFuncionario);
+        System.out.printf("Salario = U$ %.2f" , salarioFuncionario);
+
+
+    }
+}
