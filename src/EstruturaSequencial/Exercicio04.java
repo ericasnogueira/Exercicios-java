191a27ac0758a95a73c5d084a9d8a8fe96d57195
erica nogueira <ericasnogueira10@gmail.com>
2022-08-18 15:22:38 -0300

    Corrigindo o nome do arquivo

--- src/EstruturaSequencial/Exercicio04.java
+++ src/EstruturaSequencial/Exercicio04.java
@@ -3,7 +3,7 @@ package EstruturaSequencial;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Exericio04 {
+public class Exercicio04 {
     public static void main(String[] args) {
         /*
         Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por
