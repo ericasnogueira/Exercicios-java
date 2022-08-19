1ffc4b8dd609ba7dfe33ee19acb9ccac3ddf99be
erica nogueira <ericasnogueira10@gmail.com>
2022-08-19 09:07:50 -0300

    Exercicio02

---
+++ src/EstrututaCondicional/Exercicio02.java
@@ -0,0 +1,23 @@
+package EstrututaCondicional;
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+        /*
+            Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
+         */
+        Scanner entrada = new Scanner(System.in);
+
+        int numero;
+
+        numero = entrada.nextInt();
+
+        if(numero % 2 == 0){
+            System.out.println("PAR");
+        } else {
+            System.out.println("IMPAR");
+        }
+
+    }
+}
