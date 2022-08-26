7ba3f084e5c85e90eb00bb59b050d5382f6126e9
erica nogueira <ericasnogueira10@gmail.com>
2022-08-26 20:41:33 -0300

    ExercicioWhile03

---
+++ src/EstruturaRepetitivas/Exercicio03While.java
@@ -0,0 +1,39 @@
+package EstruturaRepetitivas;
+
+import java.util.Scanner;
+
+public class Exercicio03While {
+    public static void main(String[] args) {
+        /*
+        Um Posto de combustíveis deseja determinar qual de seus produtos tem a preferência de seus clientes. Escreva
+        um algoritmo para ler o tipo de combustível abastecido (codificado da seguinte forma: 1.Álcool 2.Gasolina 3.
+        Diesel 4.Fim). Caso o usuário informe um código inválido (fora da faixa de 1 a 4) deve ser solicitado um
+        novo código (até que seja válido). O programa será encerrado quando o código informado for o número 4.
+        Deve ser escrito a mensagem: "MUITO OBRIGADO" e a quantidade de clientes que abasteceram cada tipo de
+        combustível, conforme exemplo.
+         */
+
+        Scanner entrada = new Scanner(System.in);
+
+        int codigoProduto = entrada.nextInt();
+        int quantidade =1;
+        int alcool = 0;
+        int gasolina = 0;
+        int diesel = 0;
+        while (codigoProduto != 4){
+                //alcool
+                if(codigoProduto == 1 ) {
+                    alcool += quantidade;
+                } else if(codigoProduto ==2) {// gasolina
+                    gasolina += quantidade;
+                } else if (codigoProduto == 3) {// diesel
+                    diesel += quantidade;
+                }
+            codigoProduto = entrada.nextInt();
+        }
+        System.out.println("MUITO OBRIGADO");
+        System.out.println("Alcool : " + alcool);
+        System.out.println("Gasolina : " + gasolina)  ;
+        System.out.println("Diesel : " + diesel);
+    }
+}
