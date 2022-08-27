2e8261509a37ce8708d91dfe5d124d0d0f8e500e
erica nogueira <ericasnogueira10@gmail.com>
2022-08-27 10:13:21 -0300

    ExercicioParaFor01

--- src/EstruturaRepetitivas/ExercicioFor01.java
+++ src/EstruturaRepetitivas/ExercicioFor01.java
@@ -1,20 +1,20 @@
-package EstrututaCondicional;
+package EstruturaRepetitivas;
 
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Exercicio12 {
+public class ExercicioFor01 {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         /*
         Faça um programa que leia 6 valores. Estes valores serão somente negativos ou positivos
         (desconsidere os valores nulos). A seguir, mostre a quantidade de valores positivos digitados.
          */
-        Locale.setDefault(Locale.US);
         Scanner entrada = new Scanner(System.in);
         int positivo =0 ;
 
-        for(int i =0; i< 6 ; i++){
-            int numero = entrada.nextInt();
+        for(int i = 0; i< 6 ; i++){
+            double numero = entrada.nextDouble() ;
             if (numero > 0){
                 positivo += 1;
             }
