ed88b3c3194d3be2c661eb7c4626fb173cd0a181
erica nogueira <ericasnogueira10@gmail.com>
2022-08-28 18:05:45 -0300

    ExercicioFor04

---
+++ src/EstruturaRepetitivas/ExercicioFor04.java
@@ -0,0 +1,32 @@
+package EstruturaRepetitivas;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExercicioFor04 {
+    public static void main(String[] args) {
+        /*
+            Leia 1 valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de teste
+             consiste de 3 valores reais, cada um deles com uma casa decimal. Apresente a média ponderada para
+              cada um destes conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso
+               3 e o terceiro valor tem peso 5.
+         */
+        Locale.setDefault(Locale.US);
+        Scanner entrada = new Scanner(System.in);
+
+
+
+        int numero = entrada.nextInt();
+        for (int i = 0; i < numero; i++){
+            double teste1 = entrada.nextDouble();
+            double teste2 = entrada.nextDouble();
+            double teste3 = entrada.nextDouble();
+
+            double total = ((teste1 * 2) + (teste2 * 3) + (teste3 * 5 )) / 10; //2+3+5
+
+            System.out.printf("%.1f", total);
+        }
+
+        entrada.close();
+    }
+}
