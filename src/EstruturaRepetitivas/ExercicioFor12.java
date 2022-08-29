6f013f244229fa8b262ddb5038f39ac13b0b0d25
erica nogueira <ericasnogueira10@gmail.com>
2022-08-29 15:19:50 -0300

    ExercicioFor12

---
+++ src/EstruturaRepetitivas/ExercicioFor12.java
@@ -0,0 +1,20 @@
+package EstruturaRepetitivas;
+
+import java.util.Scanner;
+
+public class ExercicioFor12 {
+    public static void main(String[] args) {
+        /*
+        Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha,
+         inclusive o X, se for o caso.
+         */
+        Scanner entrada = new Scanner(System.in);
+        int numero = entrada.nextInt();
+
+        for(int i =1; i<= numero; i ++){
+            if(i % 2 ==1){
+                System.out.println(i);
+            }
+        }
+    }
+}
