6cf191cb2e1b10723f7fd70cc9665584ee2be9fa
erica nogueira <ericasnogueira10@gmail.com>
2022-08-29 11:08:51 -0300

    ExercicioFor08

--- src/EstruturaRepetitivas/ExercicioFor08.java
+++ src/EstruturaRepetitivas/ExercicioFor08.java
@@ -1,7 +1,25 @@
 package EstruturaRepetitivas;
 
+import java.util.Scanner;
+
 public class ExercicioFor08 {
     public static void main(String[] args) {
-        
+        /*
+        Fazer um programa para ler um número inteiro positivo N. O programa deve então mostrar na tela N linhas,
+        começando de 1 até N. Para cada linha, mostrar o número da linha, depois o quadrado e o cubo do valor, conforme
+        exemplo
+         */
+        Scanner entrada = new Scanner(System.in);
+
+        int numero = entrada.nextInt();
+        for(int i = 1; i<= numero;i ++){
+            int a;
+            a = i;
+            int c = a * i ;
+            int b = i * c;
+
+            System.out.println(i + " " + c + " " + b);
+
+        }
     }
 }
