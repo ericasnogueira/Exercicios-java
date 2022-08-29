cf2e3b2b8d45ea427f0d3b368d65aeee8615f169
erica nogueira <ericasnogueira10@gmail.com>
2022-08-29 15:46:36 -0300

    ExercicioWhile06

---
+++ src/EstruturaRepetitivas/ExercicioWhile06.java
@@ -0,0 +1,24 @@
+package EstruturaRepetitivas;
+
+import java.util.Scanner;
+
+public class ExercicioWhile06 {
+    public static void main(String[] args) {
+        /*
+        Leia um valor inteiro X. Em seguida apresente os 6 valores ímpares consecutivos a partir de X,
+        um valor por linha, inclusive o X ser for o caso.
+         */
+        Scanner entrada = new Scanner(System.in);
+        int numero = entrada.nextInt();
+        int cont=0 ;
+
+        while (cont < 6){
+            if ( numero % 2 ==1){
+                System.out.println(numero);
+                cont ++;
+            }
+            numero ++;
+        }
+
+    }
+}
