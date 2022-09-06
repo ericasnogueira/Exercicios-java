c4dc40f48720adc9c9feaf2ae806e9b4ad6be3a6
erica nogueira <ericasnogueira10@gmail.com>
2022-09-05 21:11:14 -0300

    Exercicio014

--- src/EstrututaCondicional/Exercicio014.java
+++ src/EstrututaCondicional/Exercicio014.java
@@ -1,2 +1,36 @@
-package EstrututaCondicional;public class Exercicio014 {
+package EstrututaCondicional;
+
+import java.util.Scanner;
+
+public class Exercicio014 {
+    public static void main(String[] args) {
+        /*
+        Neste problema, você deverá ler 3 palavras que definem o tipo de animal possível segundo o esquema abaixo,
+         da esquerda para a direita.  Em seguida conclua qual dos animais seguintes foi escolhido, através das
+         três palavras fornecidas.
+         */
+        Scanner entrada = new Scanner(System.in);
+
+        String animal1 = entrada.next();
+        String animal2 = entrada.next();
+        String animal3 = entrada.next();
+
+        if(animal1.equals("vertebrado") && animal2.equals("ave") && animal3.equals("carnivoro")){
+            System.out.println("aguia");
+        } else if (animal1.equals("vertebrado") && animal2.equals("ave") && animal3.equals("onivoro")) {
+            System.out.println("pomba");
+        } else if (animal1.equals("vertebrado") && animal2.equals("mamifero") && animal3.equals("onivoro")) {
+            System.out.println("homem");
+        } else if (animal1.equals("vertebrado") && animal2.equals("mamifero") && animal3.equals("herbivero")) {
+            System.out.println("vaca");            
+        } else if (animal1.equals("invertebrado") && animal2.equals("inseto") && animal3.equals("hematofago")) {
+            System.out.println("pulga");
+        } else if (animal1.equals("invertebrado") && animal2.equals("inseto") && animal3.equals("herbivoro")) {
+            System.out.println("lagarta");
+        } else if (animal1.equals("invertebrado") && animal2.equals("anelideo") && animal3.equals("hematofago")) {
+            System.out.println("sanguessuga");
+        } else if (animal1.equals("invertebrado") && animal2.equals("anelideo") && animal3.equals("onivoro")) {
+            System.out.println("minhoca");
+        }
+    }
 }
